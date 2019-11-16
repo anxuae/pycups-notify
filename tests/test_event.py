7845fd34221193070ff40c51cea724aa938c3682
anxuae <anxuae-prog@yahoo.fr>
2019-11-16 15:01:41 +0100

    Add unit tests

---
+++ tests/test_event.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from cups_notify import event
+
+
+def test_event_parsing(evts):
+    evt = event.CupsEvent(evts[0])
+
+    assert evt.guid > 0
+    assert evt.title
+    assert evt.description
+    assert evt.timestamp < datetime.now()
